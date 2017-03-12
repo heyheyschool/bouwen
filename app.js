745ab0a96c00081a208c5b8fc2d656c30a86032a
heyhey <fruitsensatie@live.nl>
2017-03-12 17:02:19 +0100

    Backup files

---
+++ app.js
@@ -0,0 +1,169 @@
+var todoApp = angular.module('todoApp', ['todoApp.filters'])
+//'ui, ui.filters'
+
+todoApp.controller('todoController', function($scope) {
+	//HIER KOMT JE INHOUD :'D
+  
+
+	$scope.category = [
+	{name: "Groceries", color: "red"},
+	{name: "Work", color: "blue"},
+	{name: "School", color: "green"},
+	{name: "Lunch", color: "yellow"}];
+ 	
+ 	$scope.filters = {};
+
+	var localItems = JSON.parse(localStorage.getItem("items"));
+
+	if(localItems != undefined && localItems.length>0) {
+		$scope.items = localItems;
+	}
+	else {
+		$scope.items = [
+			{name:"Milk", checked:false, category:"Groceries"},
+			{name:"Eggs", checked:false, category:"Groceries"},
+			{name:"Cheese", checked:true, category:"Groceries"},
+			{name:"Do Homework", checked:false, category:"School"},
+			{name:"Buy pencils", checked:true, category:"School"},
+			{name:"Copy code from codebin", checked:false, category:"School"},
+			{name:"Make an Angular test", checked:false, category:"Work"}
+		];
+	}
+
+
+
+//veranderd
+	$scope.deleteItem = function(item) {
+
+		var index = $scope.items.indexOf(item);
+ 	 	$scope.items.splice(index, 1); 
+
+		localStorage.setItem("items", JSON.stringify($scope.items));
+	};
+
+	$scope.saveNewItem = function() {
+		console.log($scope.newItem);
+
+		$scope.items.push({
+			name: $scope.newItem.name,
+			category: $scope.newItem.category,
+			checked: false
+		});
+
+		$scope.newItem = {};
+
+		localStorage.setItem("items", JSON.stringify($scope.items));
+	};
+
+	$scope.updateItem = function(item) {
+		item.updating=false;
+
+		localStorage.setItem("items", JSON.stringify($scope.items));
+	};
+
+
+	/*$scope.uniqueItems = function(){
+		var test = $scope.items.
+		console.log(test);
+		uniqueArray = jQuery.unique($scope.items.category);
+		console.log(uniqueArray);
+	return uniqueArray;
+
+	}
+
+
+	//gekopieerde code
+
+	/*angular.module('ui.filters').filter('unique', function () {
+
+  return function (items, filterOn) {
+
+    if (filterOn === false) {
+      return items;
+    }
+
+    if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
+      var hashCheck = {}, newItems = [];
+
+      var extractValueToCompare = function (item) {
+        if (angular.isObject(item) && angular.isString(filterOn)) {
+          return item[filterOn];
+        } else {
+          return item;
+        }
+      };
+
+      angular.forEach(items, function (item) {
+        var valueToCheck, isDuplicate = false;
+
+        for (var i = 0; i < newItems.length; i++) {
+          if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
+            isDuplicate = true;
+            break;
+          }
+        }
+        if (!isDuplicate) {
+          newItems.push(item);
+        }
+
+      });
+      items = newItems;
+    }
+    return items;
+  };
+});*/
+
+
+
+ /* $scope.selected = {};
+  
+  $scope.category = [{id:"1",name:"work"}];
+
+  $scope.data =  [
+    {"id":"113000",
+      "values":
+          [{$scope.category.id = 1},
+           {"note":"Loonstroken", "checked": false}]
+    },
+    {"id":"113002",
+      "values":
+          [{"category":"school"},
+           {"note":"Blok H", "checked": true}]
+    }
+    ];
+  
+  $scope.$watch('selected.id', function(id){
+    delete $scope.selected.value;
+    angular.forEach($scope.data, function(attr){
+      if(attr.id === id){
+        $scope.selectedAttr = attr;
+      }
+    });
+      });
+
+*/
+})
+
+/*filter('unique', function() {
+  return function(items, category) {
+    var newitems =[];
+    items.forEach(function(project){
+      if(item.category === subProjectName)
+        newitems.push(project);
+    });
+    return newitems;
+  };
+});*/
+
+/*todoApp.filter('unique', function() {
+    return function (arr, field) {
+        return _.uniq(arr, function(a) { return a[field]; });
+    };
+});*/
+
+/*todoApp.filter('unique', function () {
+  return function (array) {
+  	uniqueArray = jQuery.unique(array);
+    return uniqueArray;
+  };
+});*/
